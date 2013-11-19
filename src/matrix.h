2ad01198c882c6020128a67a5369113820b781d1
Shuai YUAN <yszheda@gmail.com>
2013-11-19 15:32:14 +0800

    an attempt to optimize out unnecessary cases.

--- src/matrix.h
+++ src/matrix.h
@@ -24,15 +24,15 @@
 
 #define index(i, j, size) (((i) * (size)) + (j))
 
+//#define TILE_WIDTH 2
 #define TILE_WIDTH_ROW 2
 #define TILE_WIDTH_COL 64
-//#define TILE_WIDTH 2
 #define TILE_DEPTH 2
 
 #define SINGLE_GRID_SIZE 16384 // MAX 
 
-#define W 4
-#define NW (1 << W) /* In other words, NW equals 2 to the w-th power */
+// #define W 4
+// #define NW (1 << W) /* In other words, NW equals 2 to the w-th power */
 
 #define gf uint8_t
 
@@ -40,13 +40,8 @@
 __host__ __device__ uint8_t gf_add(uint8_t a, uint8_t b);
 __host__ __device__ uint8_t gf_sub(uint8_t a, uint8_t b);
 __host__ __device__ uint8_t gf_mul(uint8_t a, uint8_t b);
-// __host__ __device__ uint8_t gf_mul(uint8_t a, uint8_t b, uint8_t *gflog, uint8_t *gfexp);
-// __host__ __device__ uint8_t gf_mul_bit(uint8_t a, uint8_t b);
-// __host__ __device__ uint8_t gf_mul_bit(uint8_t a, uint8_t b, uint8_t *gflog, uint8_t *gfexp);
 __host__ __device__ uint8_t gf_div(uint8_t a, uint8_t b);
-// __host__ __device__ uint8_t gf_div(uint8_t a, uint8_t b, uint8_t *gflog, uint8_t *gfexp);
 __host__ __device__ uint8_t gf_pow(uint8_t a, uint8_t power);
-// __host__ __device__ uint8_t gf_pow(uint8_t a, uint8_t power, uint8_t *gflog, uint8_t *gfexp);
 __device__ void matrix_mul(uint8_t *A, uint8_t *B, uint8_t *C, int n, int p, int m);
 
 __global__ void gen_encoding_matrix(uint8_t *encodingMatrix, int row, int col);
