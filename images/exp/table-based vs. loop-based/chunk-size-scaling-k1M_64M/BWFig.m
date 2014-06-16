a0c5af74895d5b476800913cf36a1489d76bba97
Shuai YUAN <yszheda@gmail.com>
2014-06-16 20:44:11 +0800

    update images

--- images/exp/table-based vs. loop-based/chunk-size-scaling-k1M_64M/BWFig.m
+++ images/exp/table-based vs. loop-based/chunk-size-scaling-k1M_64M/BWFig.m
@@ -4,8 +4,9 @@ hold on;
 plot(chunkSizeScaling(1,2:end), chunkSizeScaling(3,2:end), 'Color', 'g', 'DisplayName', 'k=8,n=10(Table-based)');
 plot(chunkSizeScaling(1,2:end), chunkSizeScaling(4,2:end), 'Color', 'm', 'DisplayName', 'k=16,n=18(Table-based)');
 plot(chunkSizeScaling(1,2:end), chunkSizeScaling(5,2:end), 'Color', 'r', 'DisplayName', 'k=4,n=6(Loop-based)');
-plot(chunkSizeScaling(1,2:end), chunkSizeScaling(6,2:end), 'Color', 'c', 'DisplayName', 'k=8,n=10(Loop-based)');
+plot(chunkSizeScaling(1,2:end), chunkSizeScaling(6,2:end), 'Color', 'k', 'DisplayName', 'k=8,n=10(Loop-based)');
+plot(chunkSizeScaling(1,2:end), chunkSizeScaling(7,2:end), 'Color', 'c', 'DisplayName', 'k=16,n=18(Loop-based)');
 hold off
-legend('k=4,n=6(Table-based)', 'k=8,n=10(Table-based)', 'k=16,n=18(Table-based)', 'k=4,n=6(Loop-based)', 'k=8,n=10(Loop-based)', 'Location', 'Best');
+legend('k=4,n=6 (Table-based)', 'k=8,n=10 (Table-based)', 'k=16,n=18 (Table-based)', 'k=4,n=6 (Loop-based)', 'k=8,n=10 (Loop-based)', 'k=16,n=18 (Loop-based)', 'Location', 'Best');
 xlabel('chunk size (MB)');
 ylabel('effective bandwidth (MB/s)');
